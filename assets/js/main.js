b054170afd6de7cf755e30e24b2d42e96bb88485
MassimoRomano <romano93m@gmail.com>
2024-02-27 17:04:57 +0100

    add markup and fix error

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,27 @@
-console.log('Funziona');
-
-
 //ho inserito un array per le img
-const imgSlide = [
+
+const slides = [
     '01.webp',
     '02.webp',
     '03.webp',
     '04.webp',
     '05.webp',
-]
+];
 
+let imgActive = 0;
 
+const slidesElement = document.querySelector('.container-img');
 
-const imgActive = 0
-
-const slideElement = document.querySelector('.container-img')
-
-console.log(imgSlide, imgActive, slideElement);
-
-
+console.log(slides, imgActive, slidesElement);
 
 //inserisco un ciclo for per scorrere nelle img
+for (let i = 0; i < slides.length; i++) {
+    const slide = slides[i];
+    console.log(slide);
 
-for(let i = 0 ; i < imgSlide.leght ; i++) {
-    const slide = imgSlide[i];
-    console.log('slide');
+//inserisco un markup per selezionare le img e dargli la class active 
+    let slideMarkup = `<img class="${i === imgActive ? 'active' : ''}" src="./assets/img/${slide}" alt="">`;
+    console.log(slideMarkup);
 
-
-//inserisco un markup per selezionare le img dopo 
-    const slideMarkup = `<img class="${i === imgActive ? 'active' : ''}" src="./assets/img/${slide}" alt="">`
-
-    console.log('slideMarkup');
-
+    slidesElement.insertAdjacentHTML('beforeend', slideMarkup);
 }
