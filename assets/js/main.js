1ec0b2dafb9057891cd49579e8ebc166ce98727b
MassimoRomano <romano93m@gmail.com>
2024-02-27 15:57:10 +0100

    fix html and css for button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
 console.log('Funziona');
+
+
+//
+const imgSlide = [
+    '01.webp',
+    '02.webp',
+    '03.webp',
+    '04.webp',
+    '05.webp',
+]
+
+const imgActive = 0
+
+const slideElement = document.getElementsByClassName('slide')
+
+console.log(imgSlide, imgActive, slideElement);
