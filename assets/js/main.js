adb133811556461390c3f8a93f165e61893eda27
MassimoRomano <romano93m@gmail.com>
2024-02-27 16:30:55 +0100

    add cycle for and markup

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 console.log('Funziona');
 
 
-//
+//ho inserito un array per le img
 const imgSlide = [
     '01.webp',
     '02.webp',
@@ -10,8 +10,26 @@ const imgSlide = [
     '05.webp',
 ]
 
+
+
 const imgActive = 0
 
-const slideElement = document.getElementsByClassName('slide')
+const slideElement = document.querySelector('.container-img')
 
 console.log(imgSlide, imgActive, slideElement);
+
+
+
+//inserisco un ciclo for per scorrere nelle img
+
+for(let i = 0 ; i < imgSlide.leght ; i++) {
+    const slide = imgSlide[i];
+    console.log('slide');
+
+
+//inserisco un markup per selezionare le img dopo 
+    const slideMarkup = `<img class="${i === imgActive ? 'active' : ''}" src="./assets/img/${slide}" alt="">`
+
+    console.log('slideMarkup');
+
+}
